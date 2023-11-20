ed57da7cee34447759052ee0b3e640a1cddf49db
hare <washi79washi@gmail.com>
2023-11-20 14:48:55 +0900

    fix: マイグレーションファイルをもとに戻す(User)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->comment('投稿');
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
