a3f5d1c7cc367c85e7328d9018eb3ae7caee123b
hare <washi79washi@gmail.com>
2023-11-21 17:25:57 +0900

    feat: 関連テーブル(likes)のモデルのリレーション定義

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'likes')->withTimestamps();
+    }
 }
