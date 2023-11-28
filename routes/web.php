cd085cc90ac96d74c9f0b5d6c1995d0eb783ab96
hare <washi79washi@gmail.com>
2023-11-28 17:13:03 +0900

    refactor: laravel初期コードのwelcomeページとルーティング削除

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', function () {
-    return view('welcome');
-});
-
 //投稿一覧
 Route::get('', [PostController::class, 'index'])->name('post.index');
 //新規投稿
