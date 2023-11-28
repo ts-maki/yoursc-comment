71153c6f2415867740737c2059cb2d417b96d455
hare <washi79washi@gmail.com>
2023-11-28 10:17:33 +0900

    feat: アカウント削除できるようにする

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -47,7 +51,16 @@ class ProfileController extends Controller
         ]);
 
         $user = $request->user();
-
+        // $posts->likes()->detach();
+        $user->likes()->detach();
+        
+        //外部キー制約を一時的に無効化
+        Schema::disableForeignKeyConstraints();
+        $posts = Post::where('user_id', $user->id)->delete();
+        $comments = Comment::where('user_id', $user->id)->delete();
+        //外部キー制約を一時的に有効化
+        Schema::enableForeignKeyConstraints();
+    
         Auth::logout();
 
         $user->delete();
