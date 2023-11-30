a1abdebb5d38a53da91c816016b9436852eaf383
hare <washi79washi@gmail.com>
2023-11-30 15:27:56 +0900

    feat: 共通のheadタグ内の編集

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,27 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="" />
+    <meta name="description" content="みんなが投稿できる掲示板" />
     <meta name="format-detection" content="telephone=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('/favicon.ico') }}" />
     <link rel="apple-touch-icon" href="{{ asset('/favicon.ico') }}" />
-    <meta property="og:site_name" content="" />
+    <meta property="og:site_name" content="{{ config('app.name', 'みんなの掲示板') }}" />
     {{-- TODO 本番環境のURLにする --}}
-    <meta property="og:url" content="URL" />
+    <meta property="og:url" content="{{ config('app.url') }}" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="" />
-    <meta property="og:description" content="ページの説明" />
+    <meta property="og:title" content="{{ config('app.name', 'みんなの掲示板') }}" />
+    <meta property="og:description" content="みんなが投稿できる掲示板" />
     <meta property="og:locale" content="ja_JP" />
     {{-- TODO 本番環境のシェア用画像のURLを絶対パスで指定する --}}
-    <meta property="og:image" content="URL" />
+    <meta property="og:image" content="{{ asset('images/friends.png') }}" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:description" content="" />
+    <meta name="twitter:description" content="みんなが投稿できる掲示板" />
     {{-- TODO Twitter専用の記述です。シェアされた時に表示したい画像を絶対パスで設定 --}}
-    <meta name="twitter:image:src" content="URL" />
+    <meta name="twitter:image:src" content="{{ asset('images/friends.png') }}" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css',])
-    <title>みんなの掲示板</title>
+    <title>{{ config('app.name', 'みんなの掲示板') }}</title>
 </head>
 
 <body>
