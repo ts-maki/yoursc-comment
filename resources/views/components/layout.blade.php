d59d64b62c5e2e0465ed09aa6d026a4b07e01656
hare <washi79washi@gmail.com>
2023-12-01 10:54:46 +0900

    fix: 本番環境でエラーとなっているcss読み込み対応

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,6 @@
     <meta name="twitter:description" content="みんなが投稿できる掲示板" />
     {{-- TODO Twitter専用の記述です。シェアされた時に表示したい画像を絶対パスで設定 --}}
     <meta name="twitter:image:src" content="{{ asset('images/friends.png') }}" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css',])
     <title>{{ config('app.name', 'みんなの掲示板') }}</title>
 </head>
